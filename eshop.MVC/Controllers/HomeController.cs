f74b47f8f9434b4ef88e24189ace857edd8f36c9
murat.sivil <murat.sivil@outlook.com>
2024-07-05 11:06:32 +0300

    Add project files.

---
+++ eshop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using eshop.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace eshop.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            /*
+             * There are too many reasons that you need to change some feature;
+             * 1. New product property
+             * 2. Where should come products from (DB, API, EXCEL)
+             * 3. How products comes (EF, ADO, HttpClient) // productService is responsible for this.
+             */
+            var productService = new ProductService();
+            var products = productService.GetProductCardResponses();
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eshop.MVC/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using eshop.MVC.Models;
+
+namespace eshop.MVC
+{
+    public class ProductService
+    {
+        private List<ProductCardResponse> _products;
+        public ProductService()
+        {
+            _products = new List<ProductCardResponse>()
+            {
+                new(){ Id= 1, Name="POCO", Description="8GB RAM", Price=10000 },
+                new(){ Id= 2, Name="Samsung", Description="16GB RAM", Price=17000 },
+                new(){ Id= 3, Name="IPAD", Description="8GB RAM", Price=25000 },
+                new(){ Id= 4, Name="Homend", Description="8GB RAM", Price=5000 },
+            };
+        }
+        public List<ProductCardResponse> GetProductCardResponses()
+        {
+            return _products;
+        }
+    }
+}
